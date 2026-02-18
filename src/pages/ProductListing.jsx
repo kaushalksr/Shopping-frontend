64c1e89c3337016b8345f3ca796db47fa4dfc475
KAUSHAL_KISHORE <66373966+kaushalksr@users.noreply.github.com>
2026-02-18 14:05:20 +0530

    chore:updated url of productListing

--- src/pages/ProductListing.jsx
+++ src/pages/ProductListing.jsx
@@ -10,7 +10,7 @@ const ProductListing = () => {
   const [sortType, setSortType] = useState("");
 
   const { data, loading, error } = useFetch(
-    `http://localhost:3000/api/products`,
+    `https://shopping-jet-two.vercel.app/api/products`,
   );
 
   const { addToCart } = useContext(CartContext);
