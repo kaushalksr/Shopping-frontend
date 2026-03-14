62589df22e43c5fc0f8dfe880917fe89379c8382
KAUSHAL_KISHORE <66373966+kaushalksr@users.noreply.github.com>
2026-03-14 18:24:50 +0530

    ui fix

--- src/pages/OrderHistory.jsx
+++ src/pages/OrderHistory.jsx
@@ -18,22 +18,13 @@ const OrderHistory = () => {
         <h3 className="text-center">ORDERS</h3>
 
         {orderData?.map((item) => (
-          <div className="row w-100 d-flex p-2 rounded-2 m-0 border border-2 border-primary text-center justify-content-center align-items-center">
-            <div className="col-lg-4 col-sm-12">
-              {item.cart.map((product) => (
-                <img
-                  className=""
-                  style={{ width: 80, height: 80,borderRadius:"50%" }}
-                  src={product.productImage}
-                />
-              ))}
-            </div>
-            <div className="col-lg-3 col-sm-12">
+          <div className="row w-100 d-flex p-2 rounded-2 m-0 border border-2 border-primary justify-content-start align-items-center">
+            <div className="col-lg-7 col-sm-12">
               <div className="fs-5 h5">
                 {item.cart.map((product) => (
-                  <p className="p-0 m-0">
-                    {product.productName} {product.size} ({product.quantity})
-                  </p>
+                  <span className="border border-3 m-1 btn fs-5 fw-medium">
+                    {product.productName} {product.size} ({product.quantity}) 
+                  </span>
                 ))}
               </div>
             </div>
